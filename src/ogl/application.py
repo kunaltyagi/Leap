388bf72637f732bc22cff79392fb94abf79c7881
Kunal Tyagi <tyagi.kunal@live.com>
2016-10-20 11:09:13 +0530

    pylinted application code

--- src/ogl/application.py
+++ src/ogl/application.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Actual implementation of application
+"""
+
 from interface import GLInterface
-import pygame
-import OpenGL
 
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.arrays import vbo
+import pygame #pylint: disable=import-error
+from OpenGL.GL import *#pylint: disable=unused-wildcard-import, redefined-builtin, wildcard-import
+from OpenGL.GLU import *#pylint: disable=unused-wildcard-import, redefined-builtin, wildcard-import
 
-class Cube(object):
+class Cube(object): #pylint: disable=too-few-public-methods
+    """
+    Draws a wireframe cube
+    """
     def __init__(self):
-        self.vertices= ((1, -1, -1), (1, 1, -1), (-1, 1, -1), (-1, -1, -1),
-                        (1, -1, 1), (1, 1, 1), (-1, -1, 1), (-1, 1, 1))
-        self.edges = ( (0,1), (0,3), (0,4),
-                (2,1), (2,3), (2,7),
-                (6,3), (6,4), (6,7),
-                (5,1), (5,4), (5,7))
+        self.vertices = ((1, -1, -1), (1, 1, -1), (-1, 1, -1), (-1, -1, -1),
+                         (1, -1, 1), (1, 1, 1), (-1, -1, 1), (-1, 1, 1))
+        self.edges = ((0, 1), (0, 3), (0, 4),
+                      (2, 1), (2, 3), (2, 7),
+                      (6, 3), (6, 4), (6, 7),
+                      (5, 1), (5, 4), (5, 7))
 
     def __call__(self):
         glBegin(GL_LINES)
@@ -27,13 +32,13 @@ class Cube(object):
 
 
 class Application(GLInterface):
+    """
+    Implementation of GLInterface
+    """
     def __init__(self):
         GLInterface.__init__(self)
         self.quadric = gluNewQuadric()
         self.cube = Cube()
-
-    def key_up(self, key):
-        pass
 
     def key_down(self, key):
         if key == pygame.K_ESCAPE:
@@ -54,20 +59,14 @@ class Application(GLInterface):
             GLInterface.key_down(self, key)
         # TODO: buffer of keyboard to poll for key up event
 
-    def mouse_button_up(self, button):
-        pass
-
-    def mouse_button_down(self, button):
-        pass
-
-    def mouse_motion(self):
-        pass
-
     def display_loop(self):
         GLInterface.display_loop(self)
         gluCylinder(self.quadric, 4, 2, 5, 25, 1)
 
 def main():
+    """
+    Function to setup and run the application
+    """
     gl_app = Application()
     gl_app.main_loop()
 
