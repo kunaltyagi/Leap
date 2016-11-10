67ba0865ec81b0f6f1d3e25b8767ae0266d805f4
Pradyumna Paruchuri <pradyumnaparuchuri@gmail.com>
2016-11-10 22:12:05 +0000

    Smoothened gesture detection by averaging.
    Added gesture class. Need to add methods for detecting gestures,
    pass gesture parameters to the next level.

--- LeapStart.py
+++ LeapStart.py
@@ -5,118 +5,166 @@
 Module to detect gestures from Leap Motion Controller
 """
 
+import inspect
 import os
 import sys
-import inspect
 
 SRC_DIR = os.path.dirname(inspect.getfile(inspect.currentframe()))
 LIB_DIR = os.path.abspath(os.path.join(SRC_DIR, './lib'))
 sys.path.insert(0, LIB_DIR)
 
 import Leap #pylint: disable=import-error, wrong-import-position
-#from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
+
+
+
+class gestures():
+	parameters[]
+	
+	def __init__(self, start, stop, details=[]):
+		self.start_frame = start
+		self.stop_frame = stop
+		self.parameters.append(details)
+
+	def is_gesture():
+		# Check if the motion corresponds to this particular gesture
+		pass
+		
+	def gesture_details(self):
+		# Return parameters of the gesture in a tangible format
+		pass
+	
+		
+class ClearSpace(gestures):
+	
+	def is_gesture():
+		
+
+	
 class LeapListener(Leap.Listener):
-    """
-    Class to detect and pass on gestures
-    """
-    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
-    bone_names = ['Metacarpal', 'Proximal', 'INtermediate', 'Distal']
-    state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+	"""
+	Class to detect and pass on gestures
+	"""
 
-    def on_init(self, controller):
-        """
-        Args:
-            controller: ID to operate upon
-        """
-        print("Listener Initialised")
+	finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+	bone_names = ['Metacarpal', 'Proximal', 'INtermediate', 'Distal']
+	state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+	lastFrameID = 0
 
-    def on_connect(self, controller):
-        """
-        Args:
-            controller: ID to operate upon
-        """
-        print("Motion Sensor Connected")
+	def on_init(self, controller):
+		"""
+		Args:
+			controller: ID to operate upon
+		"""
+		print("Listener Initialised")
 
-        # Enable Gestures
-        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE)
-        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP)
-        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP)
-        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE)
+	def on_connect(self, controller):
+		"""
+		Args:
+			controller: ID to operate upon
+		"""
+		print("Motion Sensor Connected")
 
-    def on_disconnect(self, controller):
-        """
-        Args:
-            controller: ID to operate upon
-        """
-        print("Leap Motion disconnected")
+		# Enable Gestures
+		controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE)
+		controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP)
+		controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP)
+		controller.enable_gesture(Leap.Gesture.TYPE_SWIPE)
 
-    def on_exit(self, controller):
-        """
-        Args:
-            controller: ID to operate upon
-        """
-        print("Exited")
+	def on_disconnect(self, controller):
+		"""
+		Args:
+			controller: ID to operate upon
+		"""
+		print("Leap Motion disconnected")
 
-    def on_frame(self, controller):
-        """
-        Args:
-            controller: ID to operate upon
-        """
-        frame = controller.frame()
+	def on_exit(self, controller):
+		"""
+		Args:
+			controller: ID to operate upon
+		"""
+		print("Exited")
 
-        for hand in frame.hands:
+	def on_frame(self, controller):
+		"""
+		Args:
+			controller: ID to operate upon
+		"""
+		frame = controller.frame()
 
-            hand_type = "Left Hand" if hand.is_left else "Right Hand"
-                #print("Hand type: %s, palm x velocity: %s," %(hand_type, hand.palm_velocity.x))
-                #print("Hand type: %s, palm normal: %s, palm position: %s"
-                    #%(hand_type, hand.palm_normal, hand.palm_position))
+		for hand in frame.hands:
 
-        # Gesture detection
+			hand_type = "Left Hand" if hand.is_left else "Right Hand"
+				#print("Hand type: %s, palm x velocity: %s," %(hand_type, hand.palm_velocity.x))
+				#print("Hand type: %s, palm normal: %s, palm position: %s"
+					#%(hand_type, hand.palm_normal, hand.palm_position))
 
-        gesture_detected = None
-        if len(frame.hands) == 2:
-            left, right = frame.hands.leftmost, frame.hands.rightmost
-            rel_x_velocity = right.palm_velocity.x - left.palm_velocity.x
-            rel_orient = left.palm_normal.x*right.palm_normal.x
+		def processFrame(frame, weight, hand_count={}):
+			if( not frame.is_valid):
+				return hand_count
+			count = len(frame.hands)
+			if count in hand_count.keys():
+				hand_count[count] += weight + (1-weight)*hand_count[count]
+			else:
+				hand_count[count] = weight
+			return hand_count
 
-            if rel_orient < 0 and rel_x_velocity > 100:
-                gesture_detected = "Clear Space"
-                # Define parameters to characterise the gesture.
+		def HandCount(self, controller, window=10):
+			no_of_hands = {0:0, 1:0, 2:0}
+			for i in range(window):
+				no_of_hands = processFrame(controller.frame(window-i), 0.9, no_of_hands)
+			comp = lambda x: no_of_hands[x]
+			return max(no_of_hands, key=comp)
 
+		# Gesture detection
 
-        if len(frame.hands) == 1:
-            pointing_fingers = frame.fingers.extended()
+		# Add additional gestures here as keys. Each gestures's parameters
+		# are stored in its respective value.
+		gestures = {'clearSpace':[], 'point':[]}
 
-            if not pointing_fingers.is_empty:
-                gesture_detected = "Point"
-                forward_finger = pointing_fingers.frontmost
-                # Return the position on screen being pointed by the forward most finger
+		hand_count = HandCount(self, controller)
+		if hand_count == 2:
+			left, right = frame.hands.leftmost, frame.hands.rightmost
+			rel_x_velocity = right.palm_velocity.x - left.palm_velocity.x
+			rel_orient = left.palm_normal.x*right.palm_normal.x
 
-        if gesture_detected == "Clear Space":
-            print("1 Current Gesture: %s" %(gesture_detected))
-            # Send the relevant parameters across to openGL
-            pass
-        elif gesture_detected == "Point":
-            print("Current Gesture: %s, fingertype: %s " %(gesture_detected, forward_finger.type))
+			if rel_orient < 0 and rel_x_velocity > 100:
+				gesture_detected = "Clear Space"
+				# Define parameters to characterise the gesture.
+				
+		elif hand_count == 1:
+			pointing_fingers = frame.fingers.extended()
+
+			if not pointing_fingers.is_empty:
+				gesture_detected = "Point"
+				forward_finger = pointing_fingers.frontmost
+				# Return the position on screen being pointed by the forward most finger
+
+		if gesture_detected == "Clear Space":
+			print("Frame: %d Current Gesture: %s" %(frame.id, gesture_detected))
+			# Send the relevant parameters across to openGL
+		elif gesture_detected == "Point":
+			print("Frame: %d Current Gesture: %s, fingertype: %s "
+			%(frame.id, gesture_detected, self.finger_names[forward_finger.type]))
 
 
 def main():
-    """
-    Runs the application
-    """
-    listener = LeapListener()
-    controller = Leap.Controller()
+	"""
+	Runs the application
+	"""
+	listener = LeapListener()
+	controller = Leap.Controller()
 
-    controller.add_listener(listener)
+	controller.add_listener(listener)
 
-    print("Press enter to quit")
-    try:
-        sys.stdin.readline()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        controller.remove_listener(listener)
+	print("Press enter to quit")
+	try:
+		sys.stdin.readline()
+	except KeyboardInterrupt:
+		pass
+	finally:
+		controller.remove_listener(listener)
 
 if __name__ == '__main__':
-    main()
+	main()
